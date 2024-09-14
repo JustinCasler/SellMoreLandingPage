d3dc99e1d9ef8d867a6c75217e9f493839446187
JustinCasler <justinlcasler@gmail.com>
2024-09-14 11:25:15 -0400

    fix email

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -1,9 +1,7 @@
-import axios from "axios";
-import { DetailedHTMLProps, FormEvent, HTMLAttributes, useState } from "react";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 import { Button } from "./Button";
-import { Input } from "./Input";
 
 export const WaitlistForm = (
   props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
@@ -12,44 +10,27 @@ export const WaitlistForm = (
 ) => {
   const { id, ...divProps } = props;
 
-  const [email, setEmail] = useState("");
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const email = "developedlastweek@gmail.com";
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setEmailSubmitted(true);
-    const res = await axios.post("/api/waitlist", { email });
-    setEmail("");
-    window.alert(`Email "${res.data.email}" submitted!`);
-  };
-
-  const EmailMessage = () => (
-    <p className="mt-3 text-sm text-light">
-      {emailSubmitted
-        ? "We'll keep you posted!"
-        : "Be the first to experience SellMore."}
-    </p>
-  );
+  const mailtoLink = `mailto:${email}?subject=SellMore%20|`;
 
   return (
     <div {...divProps} className={twMerge("col", divProps.className)}>
-      <form className="relative gap-2 md:gap-4 row" onSubmit={handleSubmit}>
-        <Input
-          type="email"
-          className="w-full"
-          placeholder="richard.hendricks@piedpiper.com"
-          aria-label="Your email"
-          name="email"
+      <div className="relative gap-2 md:gap-4 row">
+        {/* Display the email with rounded rectangle background */}
+        <p
           id={id}
-          onChange={(event) => setEmail(event.target.value)}
-          value={email}
-          required
-        />
-        {/* Line beneath label to hide outline */}
-        <div className="absolute top-0 w-[4.3rem] h-[2px] bg-strong left-3" />
-        <Button type="submit">Waitlist</Button>
-      </form>
-      <EmailMessage />
+          className="px-4 py-2 bg-custom-light-lighter rounded-lg text-lg text-gray-800"
+        >
+          {email}
+        </p>
+        <Button
+          type="button"
+          onClick={() => (window.location.href = mailtoLink)}
+        >
+          Email Us
+        </Button>
+      </div>
     </div>
   );
 };
