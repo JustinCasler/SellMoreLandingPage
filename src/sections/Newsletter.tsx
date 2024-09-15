972d02516dae264cac095cdde514145bec07d757
JustinCasler <justinlcasler@gmail.com>
2024-09-15 19:28:01 -0400

    prettier

--- src/sections/Newsletter.tsx
+++ src/sections/Newsletter.tsx
@@ -17,7 +17,9 @@ export const Newsletter = () => {
         <Background />
         <div className="gap-6 text-center md:text-left col md:w-1/2 ">
           <h2 className="text-3xl font-bold text-gray-100">More Questions?</h2>
-          <p className="text-lg text-light">You can reach us here, feel free to ask away!</p>
+          <p className="text-lg text-light">
+            You can reach us here, feel free to ask away!
+          </p>
           <WaitlistForm id="newsletter-waitlist" />
         </div>
       </Card>
